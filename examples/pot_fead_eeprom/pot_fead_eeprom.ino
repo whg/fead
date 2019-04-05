4dde0b5bb1ee1ad57083d00d7f7948331f558993
Star Holden <star@holdendesign.co.uk>
2019-04-05 20:12:32 +0100

    added eeprom set and get
    need to add slave.setAddress to the set

---
+++ examples/pot_fead_eeprom/pot_fead_eeprom.ino
@@ -0,0 +1,33 @@
+#include "fead.hpp"
+#include <EEPROM.h>
+#include <avr/wdt.h>
+
+enum vocab {
+  LED,
+  LED2,
+  TEMP,
+  setAddress,
+};
+
+#include "BubbleTree.hpp"
+
+fead::Slave<vocab> slave;
+
+bool ledState = false;
+
+BubbleTree tree(LED_BUILTIN, 10, A0);
+
+
+void setup() {
+
+  slave.open(0);
+  slave.setAddress(EEPROM.read(0));
+  slave.setDePin(2);
+  slave.setHandler(&tree);
+}
+
+void loop() {
+  slave.update();
+  tree.update();
+}
+
