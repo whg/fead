4cba76aa8308b2d8dccc5144fd7a6b617a6a360b
Will Gallia <will@stillatplay.com>
2019-11-11 16:34:42 +0000

    switch case helpers

--- src/fead/defs.hpp
+++ src/fead/defs.hpp
@@ -2,10 +2,11 @@
 
 #define CREATE_FEAD_TYPES(prefix, vocab)						\
 	using prefix##Master  = fead::MasterT<vocab>;				\
-	using prefix##Slave   = fead::SlaveT<vocab>;					\
+	using prefix##Slave   = fead::SlaveT<vocab>;				\
 	using prefix##Message = fead::MessageT<vocab>;				\
 	using prefix##Request = fead::RequestT<vocab>;				\
 	using prefix##Response = fead::ResponseT<vocab>
 
-
+#define MAKE_GET_CASE(key, value) case key: return Response(key, value)
+#define MAKE_SET_CASE(key, value, func) case key: value = req.func(); break;
 	
